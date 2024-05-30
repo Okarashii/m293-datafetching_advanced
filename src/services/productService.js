64e3c45bd443a08cb8cad09c839f6ebcbcaa6f1c
Christian Nuttli <nuttli.christian@gmail.com>
2024-05-30 15:10:06 +0200

    Created POST Function

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,13 +1,19 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export async function getProducts(category) {
-  const response = await fetch(baseUrl + "products?category=" + category);
-  if (response.ok) return response.json();
-  throw response;
+	const response = await fetch(baseUrl + "products?category=" + category);
+	if (response.ok) return response.json();
+	throw response;
 }
 
 export async function getProduct(id) {
-  const response = await fetch(baseUrl + "products/" + id);
-  if (response.ok) return response.json();
-  throw response;
+	const response = await fetch(baseUrl + "products/" + id);
+	if (response.ok) return response.json();
+	throw response;
+}
+
+export async function postProduct(product) {
+	const response = await fetch(baseUrl + "products/", { method: "POST", body: product });
+	if (response.ok) return response.json();
+	throw response;
 }
